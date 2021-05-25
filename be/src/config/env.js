a754254a286b8a0d86bfd9f244b06766597c1985
Joao Abelha <jpabelha@hotmail.com>
2021-05-25 16:15:01 +0100

    changed keys to .env and corrected bug when matching the number of coins to another

--- be/src/config/env.js
+++ be/src/config/env.js
@@ -4,4 +4,5 @@ module.exports = Object.freeze({
     port: process.env.PORT,
     cmc_api_key: process.env.CMC_API_KEY,
     crypto_compare_api_key: process.env.CRYPTO_COMPARE_API_KEY,
+    lunar_crush_api_key: process.env.LUNAR_CRUSH_KEY
 });
